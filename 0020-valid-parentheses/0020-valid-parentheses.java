b1c537e1790f6c0e7b89ba044613dab2f6d184b0
Shrushranto Rajbongshi <110803379+Shrushranto@users.noreply.github.com>
2025-07-14 23:20:59 +0530

    Time: 5 ms (5.83%), Space: 41.8 MB (47.53%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,45 +1,30 @@
 class Solution {
     public boolean isValid(String s) {
-        Deque<Character> deque = new ArrayDeque<>();
+        HashMap<Character, Character> charSeq = new HashMap<>();
+        charSeq.put(']','[');
+        charSeq.put(')','(');
+        charSeq.put('}','{');
 
-            if (s.charAt(0) == ')' || s.charAt(0) == ']' || s.charAt(0) == '}') {
-                        return false;
-            } else if (s.length() == 1) {
-                return false;
-            }
-            for (int i =0; i< s.length();i++) {
-                if (s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{') {
-                    deque.push(s.charAt(i));
-                }else if(deque.size() == 0) {
+        Stack<Character> SequenceOfChar = new Stack<>();
+
+        if(s.length() == 1){
+            return false;
+        }
+
+        for(char i=0; i<s.length(); i++){
+            if(s.charAt(i)== '(' ||s.charAt(i)== '{' || s.charAt(i)== '['){
+                SequenceOfChar.push(s.charAt(i));
+            }else{
+                if(SequenceOfChar.isEmpty()){
                     return false;
                 }
-                 else if (s.charAt(i) == ')') {
-                    if (deque.getFirst() == '(') {
-                        deque.pop();
-                    } else {
-                                               return false;
-
-                    }
+                if(SequenceOfChar.peek() != charSeq.get(s.charAt(i))){
+                    return false;
                 }
-                else if (s.charAt(i) == ']') {
-                    if (deque.getFirst() == '[') {
-                        deque.pop();
-                    } else {
-                                               return false;
 
-                    }
-                } else if (s.charAt(i) == '}') {
-                    if (deque.getFirst() == '{') {
-                        deque.pop();
-                    } else {
-                        return false;
-                    }
-                }
+                SequenceOfChar.pop();
             }
-                        if (deque.size() >0) {
-                return false;
-            }
-            System.out.println(true);
-            return true;
         }
+        return SequenceOfChar.isEmpty();
+    }
 }
